08ff386582dec21b1aa38ab80f6f38008d1def4d
Thakshila Bandara <e21050@eng.pdn.ac.lk>
2026-07-06 10:09:23 +0530

    feat: update configuration constants for OAuth and Apps Script endpoint

--- js/config.js
+++ js/config.js
@@ -8,5 +8,5 @@
 
 const CONFIG = {
   OAUTH_CLIENT_ID: "812045761718-qh784i5vtvsbna3gma0chjrp8djii6bn.apps.googleusercontent.com",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxBkl3EQEhy5vPw4-Lcn4nFHJ1Ru4f4B3QriNOOf47KuA-APS8jXqVeyowkWgedDqQ0/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzbudTLQmzMESNnQLmyE3c9gZwDz6ejzGlbfnrimfbGVwmZrSm-amm7NfnSfUDSfahX/exec",
 };
